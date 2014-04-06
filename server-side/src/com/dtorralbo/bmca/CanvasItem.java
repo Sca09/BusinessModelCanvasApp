1a51236af22435d2484c0a65c5f2ba4bab33bf1d
David Torralbo <davidtorralbo@gmail.com>
2014-04-06 16:29:35 +0200

    First server side logic and endPoints creation

---
+++ server-side/src/com/dtorralbo/bmca/CanvasItem.java
@@ -0,0 +1,58 @@
+package com.dtorralbo.bmca;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+public class CanvasItem {
+
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Long id;
+	
+	@Persistent
+	private String category;
+	
+	@Persistent
+	private String title;
+	
+	@Persistent
+	private String description;
+	
+	@Persistent
+	private String creator;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+}
